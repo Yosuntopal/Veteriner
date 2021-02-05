e3fe78e4401b27498b8a727881aeb382c16586bf
Yosuntopal <49617193+Yosuntopal@users.noreply.github.com>
2021-02-05 17:31:58 +0300

    Add files via upload

---
+++ veteriner/Data/Connection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace veteriner.Data
+{
+    class Connection
+    {
+      //Constructor Connection
+       public SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-AJC749U\SQLEXPRESS;Initial Catalog=Veteriner;Integrated Security=True");
+       //veri tabanı pathı
+
+        public void Baglan()
+        {
+            try
+            {
+                con.Open();
+            }
+            catch (Exception)
+            {
+                con.Close();
+            }
+
+        }
+
+    }
+}
