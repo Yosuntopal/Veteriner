b99e9c2eba6aa79c4b5392132fd457e99ad40aed
Yosuntopal <49617193+Yosuntopal@users.noreply.github.com>
2021-02-05 17:32:30 +0300

    Add files via upload

---
+++ veteriner/Models/Users.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace veteriner.Models
+{
+    //encapsulation 
+    public static class Users
+    {
+        public static string NameSurname { get; set; }
+    }
+}
